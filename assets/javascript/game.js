88f5ae3634d13a2d73908a655ea6ff993e9ffd23
victor politano <viant3ucf@gmail.com>
2019-06-22 06:06:05 -0400

    displayed data in html - leaving console logs for reference - game done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,58 @@
 //scoring variables
 var userWins = 0;
 var userLoses = 0;
-guessesLeft = 9;
-var guessed = 0;
+var guessesLeft = 9;
+
 
 var compPossibles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var userTypedText = document.getElementById("userTyped-text")
-var compPossiblesText = document.getElementById("compPossibles-text");
-
-
-document.onkeyup = function(event) {
-//variable to get user input
-var userTyped = event.key;
-
-//variable for computer choice
-var psychicChoice = compPossibles[Math.floor(Math.random() * compPossibles.length)];
 
 
 
-if ((userTyped === psychicChoice) && (guessesLeft > 0)) {
-    userWins++;
+document.onkeyup = function (event) {
+    //variable to get user input
+    var userTyped = event.key;
 
-    console.log("You chose letter " + userTyped);
-    console.log("The computer chose letter " + psychicChoice);
+    //variable for computer choice
+    var psychicChoice = compPossibles[Math.floor(Math.random() * compPossibles.length)];
 
-    console.log("It's a match! Your score is: " + userWins);
+    //variables for user input
+
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+  
+
+    if ((userTyped === psychicChoice) && (guessesLeft > 0)) {
+        userWins++;
+
+        console.log("You chose letter " + userTyped);
+        console.log("The computer chose letter " + psychicChoice);
+
+        console.log("It's a match! Your score is: " + userWins);
     }
-else if ((userLoses < 9) && (guessesLeft > 0)) {
+    else if ((userLoses < 9) && (guessesLeft > 0)) {
 
-    userLoses++;
-    guessesLeft--;
+        userLoses++;
+        guessesLeft--;
 
-    console.log("You chose letter " + userTyped);
-    console.log("The computer chose letter " + psychicChoice);
 
-    console.log("Not a match. Number of losses so far: " + userLoses);
-    console.log("Number of guesses left: " + guessesLeft);
+        console.log("You chose letter " + userTyped);
+        console.log("The computer chose letter " + psychicChoice);
+
+        console.log("Not a match. Number of losses so far: " + userLoses);
+        console.log("Number of guesses left: " + guessesLeft);
     }
-else {
+    else {
 
-    userLoses = 0;
-    guessesLeft = 9;
-    
-    console.log("Game over. Try again");
+        // reset the game variables 
+        userLoses = 0;
+        guessesLeft = 9;
+        userWins = 0;
+
+        console.log("Game over. Try again");
     }
 
+    winsText.textContent = "wins: " + userWins;
+    lossesText.textContent = "losses: " + userLoses;
+    guessesLeftText.textContent = "guesses left: " + guessesLeft;
 } 
