6a1abb2227c194b720b29033bbe5b6f06fc3aae3
victor politano <viant3ucf@gmail.com>
2019-07-02 05:18:19 -0400

    put random comp letter into a function not called until game loss or win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,42 @@
 var userWins = 0;
 var userLoses = 0;
 var guessesLeft = 9;
-var lettersGuessed = [];
 var lettersThisGame = [];
 
-var compPossibles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+
+var compPossibles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var psychicChoice = compPossibles[Math.floor(Math.random() * compPossibles.length)];
+
+function compGuess() {
+    psychicChoice = compPossibles[Math.floor(Math.random() * compPossibles.length)];
+}
 
 document.onkeyup = function (event) {
     //var to get user input
     var userTyped = event.key;
 
     //var to popuate array with user key press
-    lettersGuessed.push(userTyped);
+
     lettersThisGame.push(userTyped);
-
-    //var for computer choice
-
-    var psychicChoice = compPossibles[Math.floor(Math.random() * compPossibles.length)];
 
 
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var guessedThisGame = document.getElementById("lettersThisGame");
-    var guessedText = document.getElementById("guessedText");
+
 
 
     if ((userTyped === psychicChoice) && (guessesLeft > 0)) {
-        userWins++;
 
+        userWins++;
         guessesLeft = 9;
         lettersThisGame = [];
 
         console.log("You chose letter " + userTyped);
         console.log("The computer chose letter " + psychicChoice);
         console.log("It's a match! Your score is: " + userWins);
+        compGuess();
     }
     else if (guessesLeft > 0) {
 
@@ -44,7 +46,6 @@ document.onkeyup = function (event) {
 
         console.log("You chose letter " + userTyped);
         console.log("The computer chose letter " + psychicChoice);
-        console.log("Not a match. Number of losses so far: " + userLoses);
         console.log("Number of guesses left: " + guessesLeft);
     }
     else {
@@ -53,6 +54,7 @@ document.onkeyup = function (event) {
         userLoses++;
         guessesLeft = 9;
         lettersThisGame = [];
+        compGuess();
 
         console.log("Game over. Try again");
     }
@@ -61,6 +63,6 @@ document.onkeyup = function (event) {
     lossesText.textContent = "Losses: " + userLoses;
     guessesLeftText.textContent = "Guesses left: " + guessesLeft;
     guessedThisGame.textContent = "Letters guessed in this game: " + lettersThisGame;
-    guessedText.textContent = "Total letters picked: " + lettersGuessed;
+
 
 }
